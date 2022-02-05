ade17be89a71d8ea68b9d334681a117481af06e4
Vinicius Dias <vinicius@venngage.com>
2022-02-05 19:30:18 -0300

    Video 5.4

--- ordem.php
+++ ordem.php
@@ -15,10 +15,7 @@ $notas = [
     ],
 ];
 
-function ordenaNotas(array $nota1, array $nota2): int
-{
+usort($notas, function (array $nota1, array $nota2): int {
     return $nota2['nota'] <=> $nota1['nota'];
-}
-
-usort($notas, 'ordenaNotas');
+});
 var_dump($notas);
