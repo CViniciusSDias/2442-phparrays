77382907a3092b78f444c4290aa65d53680ac081
Vinicius Dias <vinicius@venngage.com>
2022-02-05 16:38:07 -0300

    Video 3.3

--- notas.php
+++ notas.php
@@ -19,3 +19,10 @@ var_dump(array_is_list($notas));
 
 echo 'Ana fez a prova:' . PHP_EOL;
 var_dump(isset($notas['Ana']));
+
+echo 'Alguém tirou 6?' . PHP_EOL;
+var_dump(in_array(6, $notas));
+
+// array_key_exists = verifica se a chave existe
+// in_array = verifica se o valor existe
+// isset = verifica se a chave existe e não é nula
