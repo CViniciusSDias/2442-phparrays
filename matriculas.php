1f81d4f0c09e2f409fce8f2240acf5f90302a236
Vinicius Dias <vinicius@venngage.com>
2022-02-05 19:10:49 -0300

    Video 5.1

--- matriculas.php
+++ matriculas.php
@@ -16,5 +16,13 @@ $novosAlunos = [
 ];
 
 $alunos2022 = [...$alunos2021, 'Carlos Vinicius', ...$novosAlunos];
+array_push($alunos2022, 'Alice', 'Bob', 'Charlie');
+$alunos2022[] = 'Luiz';
+
+array_unshift($alunos2022, 'Stephane', 'Rafaela');
+
+echo array_shift($alunos2022) . PHP_EOL;
+
+echo array_pop($alunos2022) . PHP_EOL;
 
 var_dump($alunos2022);
