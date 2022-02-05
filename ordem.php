043402ffe7b9027903c386bbc33ac1c339ba95eb
Vinicius Dias <vinicius@venngage.com>
2022-02-05 16:16:06 -0300

    Video 2.3

--- ordem.php
+++ ordem.php
@@ -17,15 +17,7 @@ $notas = [
 
 function ordenaNotas(array $nota1, array $nota2): int
 {
-    if ($nota1['nota'] > $nota2['nota']) {
-        return -1;
-    }
-
-    if ($nota2['nota'] > $nota1['nota']) {
-        return 1;
-    }
-
-    return 0;
+    return $nota2['nota'] <=> $nota1['nota'];
 }
 
 usort($notas, 'ordenaNotas');
